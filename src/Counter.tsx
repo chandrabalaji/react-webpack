cd00142c7c987fc6e75bee8b44df63d71bf55444
Balaji <balaji@efficient-works.com>
2024-03-26 14:16:59 +0530

    ynmnr

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,12 +3,10 @@ import React, { useState } from 'react'
 const Counter = () => {
   const [count, setcount] = useState(0)
   // const [val, seval] = useState(0)
-  console.log("hello world")
+  console.log('hello world')
   const name = 'ram'
 
-
   return (
-    
     <div>
       <button onClick={() => setcount((val) => val - 1)}>-</button>
       <span>{count}</span>
