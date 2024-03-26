bd0b3c8ffe40dd2a20eddb608a4e1e295c469246
Balaji <balaji@efficient-works.com>
2024-03-26 12:22:39 +0530

    husky test

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,7 +4,7 @@ const Counter = () => {
   const [count, setcount] = useState(0)
   // const [val, seval] = useState(0)
   console.log('hell world')
-  const name = 'john'
+  const name = "hohn"
 
   return (
     <div>
@@ -12,6 +12,20 @@ const Counter = () => {
       <span>{count}</span>
       <button onClick={() => setcount((val) => val + 1)}>+</button>
       {name}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum cumque
+        sit vitae ducimus fugit accusantium ut perferendis culpa possimus
+        consequatur ab perspiciatis maxime deserunt magni repellendus iste
+        numquam doloremque enim nobis minus, impedit quidem. Totam praesentium
+        esse blanditiis illo. Eaque tempora atque exercitationem reprehenderit
+        totam blanditiis neque aliquam odio, vel aperiam ex modi illo deserunt!
+        Facere fuga exercitationem velit illo laboriosam autem dolorum,
+        inventore assumenda, est esse dignissimos cupiditate atque! Consectetur
+        architecto, maiores vero libero a saepe nam. Officia veniam nostrum hic
+        assumenda consequatur quae voluptatibus voluptas, voluptatum rerum dolor
+        cumque. Deserunt unde voluptate nam? Sunt iusto numquam nihil
+        temporibus!
+      </p>
     </div>
   )
 }
