1ceec96531639d5cb77518db9238c29c31adb2ca
Balaji <balaji@efficient-works.com>
2024-03-26 12:00:32 +0530

    test3

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 
 const Counter = () => {
   const [count, setcount] = useState(0)
-  const [val, seval] = useState(0)
+  // const [val, seval] = useState(0)
   console.log('hell world')
   const name = "john"
-  
+
   return (
     <div>
       <button onClick={() => setcount((val) => val - 1)}>-</button>
       <span>{count}</span>
       <button onClick={() => setcount((val) => val + 1)}>+</button>
+      {name}
     </div>
   )
 }
